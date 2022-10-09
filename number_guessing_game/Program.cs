da00489188c7e508e5b785d7c603ad236a7f6a55
LySeriuos <drungilas.p@gmail.com>
2022-10-09 20:05:18 +0200

    updated else if statements to check if user's number is in range -5 to 5 from random number

--- number_guessing_game/Program.cs
+++ number_guessing_game/Program.cs
@@ -10,18 +10,31 @@ namespace number_guessing_game
                 "\n\tComputer will get the number and you must guess it." +
                 "\n\tYou will enter a number and computer will give you " +
                 "answer if it is too low or too high.\n\tYou will have 10 tries and if you can't get the right number you will loose!");
-            
+            int randomNumber = new Random().Next(101); // it gives the random number from 0 to 100
+            Console.WriteLine($"{randomNumber}");
+
             Console.WriteLine("Guess Your number!"); 
             int userNumber = Convert.ToInt32(Console.ReadLine()); //converting user's string input to int
 
-            int randomNumber = new Random().Next(101); // it gives the random number from 0 to 100
-            Console.WriteLine($"{randomNumber}");
+           
+            int numberRange = userNumber - randomNumber; // count difference between user input number and random number
             if (randomNumber == userNumber)
+            {
                 Console.WriteLine("You win!");
+            }
             else if (randomNumber < userNumber)
+            {
+                if (-5 <= numberRange && numberRange <= 5) // user's number is lower up to 5 or higher up to 5 random number
+                    Console.WriteLine("You are close!");
                 Console.WriteLine("Too High!");
+                
+            }
             else if (randomNumber > userNumber)
-                Console.WriteLine("Too Low!");
+            {
+                if (-5 <= numberRange && numberRange <= 5) // user's number is lower up to 5 or higher up to 5 random number
+                    Console.WriteLine("You are close!");
+                Console.WriteLine("You are close!");
+                Console.WriteLine("Too Low!");            }
         }
     }
 }
