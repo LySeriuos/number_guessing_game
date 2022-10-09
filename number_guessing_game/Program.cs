9af949afcecb2b7928b2e228b55a873b28798028
LySeriuos <drungilas.p@gmail.com>
2022-10-09 19:22:50 +0200

    added input reading line

--- number_guessing_game/Program.cs
+++ number_guessing_game/Program.cs
@@ -10,6 +10,13 @@ namespace number_guessing_game
                 "\n\tComputer will get the number and you must guess it." +
                 "\n\tYou will enter a number and computer will give you " +
                 "answer if it is too low or too high.\n\tYou will have 10 tries and if you can't get the right number you will loose!");
+            
+            Console.WriteLine("Guess Your number!"); 
+            int userNumber = Convert.ToInt32(Console.ReadLine()); //converting user's string input to int
+
+            int randomNumber = new Random().Next();
+            if (randomNumber == userNumber)
+                Console.WriteLine("Good guess");
         }
     }
 }
